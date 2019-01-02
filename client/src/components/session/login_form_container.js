b154ea6aaeb1b90fce7591d528b047e4cf1198fd
Micah Jaffe <micah.jaffe95@gmail.com>
2019-01-02 10:55:15 -0800

    Add login and signup form containers

---
+++ client/src/components/session/login_form_container.js
@@ -0,0 +1,22 @@
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { login, clearErrors } from "../../actions/session_actions";
+import SessionForm from "./session_form";
+
+const mapStateToProps = ({ errors }) => ({
+  errors: errors.session,
+  formType: "login",
+  switchText: "Don't have an account?",
+  navLink: <Link to="/signup">Create account</Link>
+});
+
+const mapDispatchToProps = dispatch => ({
+  processForm: user => dispatch(login(user)),
+  login: user => dispatch(login(user)),
+  clearErrors: () => dispatch(clearErrors())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SessionForm);
---
+++ client/src/components/session/signup_form_container.js
@@ -0,0 +1,22 @@
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { login, clearErrors } from "../../actions/session_actions";
+import SessionForm from "./session_form";
+
+const mapStateToProps = ({ errors }) => ({
+  errors: errors.session,
+  formType: "signup",
+  switchText: "Already have an account?",
+  navLink: <Link to="/login">Sign in</Link>
+});
+
+const mapDispatchToProps = dispatch => ({
+  processForm: user => dispatch(signup(user)),
+  login: user => dispatch(login(user)),
+  clearErrors: () => dispatch(clearErrors())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SessionForm);
