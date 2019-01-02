47c79a622d13ae13139c06191671460a4e968327
Micah Jaffe <micah.jaffe95@gmail.com>
2019-01-02 12:04:28 -0800

    Add auth form dialog

--- client/src/components/homepage/HelloWorld.jsx
+++ client/src/components/homepage/HelloWorld.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { HelloWorldHeader } from '../../styles/heading';
 // import Modal from '@material-ui/core/Modal';
 import SignIn from '../session/SignIn';
+import FormDialog from '../session/auth_modal';
 
 const HelloWorld = (props) => (
    <>
    <HelloWorldHeader>Hello World!!!!!!!</HelloWorldHeader>
-   <SignIn />
+   {/* <SignIn /> */}
+   <FormDialog />
+
    </>
 );
 
