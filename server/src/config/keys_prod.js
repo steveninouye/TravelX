79da770b810f0b7ca718f052b205ac7a2b8e7fab
Steven Inouye <steveninouye@msn.com>
2019-01-01 15:51:59 -1000

    Configure dev and prod keys to use module.exports syntax

--- server/src/config/keys_prod.js
+++ server/src/config/keys_prod.js
@@ -1,2 +1,4 @@
-export const dbUri = process.env.DB_URI;
-export const secretOrKey = process.env.SECRET_OR_KEY;
+module.exports = {
+   dbUri: process.env.DB_URI,
+   secretOrKey: process.env.SECRET_OR_KEY
+};
