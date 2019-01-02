103565d5c6f391edff98e2c3045abf34e7a5d1eb
Micah Jaffe <micah.jaffe95@gmail.com>
2019-01-02 11:46:50 -0800

    Add material ui signup form

--- client/src/components/homepage/HelloWorld.jsx
+++ client/src/components/homepage/HelloWorld.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { HelloWorldHeader } from '../../styles/heading';
+// import Modal from '@material-ui/core/Modal';
+import SignIn from '../session/SignIn';
 
 const HelloWorld = (props) => (
+   <>
    <HelloWorldHeader>Hello World!!!!!!!</HelloWorldHeader>
+   <SignIn />
+   </>
 );
 
 export default HelloWorld;
