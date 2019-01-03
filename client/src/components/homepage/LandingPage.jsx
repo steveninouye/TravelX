6281e7dfd10c3a981353dddda3d9fde9287bc68f
Josh Stroud <josh.e.stroud@gmail.com>
2019-01-03 11:55:31 -0800

    Add landing page feature images

--- client/src/components/homepage/LandingPage.jsx
+++ client/src/components/homepage/LandingPage.jsx
@@ -1,8 +1,40 @@
 import React, { Component } from "react";
 
+import landingPhoto from "../../img/backgrounds/city_at_dawn.jpg";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
 class LandingPage extends Component {
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div>
+        <div>Nav Bar</div>
+        <section>
+          <img src={landingPhoto} alt="" />
+        </section>
+        <section>
+          <Grid container spacing={16} justify="center">
+            <Grid item>
+              <img src="" alt="" />
+            </Grid>
+            <Grid item>
+              <Typography component="h3" color="textPrimary">
+                Choose a destination
+              </Typography>
+            </Grid>
+          </Grid>
+        </section>
+        <section>
+          <Typography variant="h2" gutterBottom>
+            h2. Heading
+          </Typography>
+          <div />
+          <div />
+          <div />
+          <div />
+        </section>
+      </div>
+    );
   }
 }
 
--- server/src/api/places_routes.js
+++ server/src/api/places_routes.js
@@ -3,22 +3,27 @@ import request from 'request';
 import rp from 'request-promise';
 import passport from 'passport';
 
-import { googleApi } from '../config/keys';
-import { location, attraction } from './temp_api';
+import {
+   googleApi
+} from '../config/keys';
+import {
+   location,
+   attraction
+} from './temp_api';
 
 const places = express.Router();
 
 places.get(
    '/:city',
-   passport.authenticate('jwt', { session: false }),
+   passport.authenticate('jwt', {
+      session: false
+   }),
    (req, res) => {
       const city = req.params;
       rp(
-         `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${city}+point+of+interest&language=en&key=${googleApi}`
-      )
+            `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${city}+point+of+interest&language=en&key=${googleApi}`
+         )
          .then((res) => {
-            const {next_page_token, results}
-            let str = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoReference}&key=${googleApi}`;
             res.json(res);
          })
          .catch((err) => {
