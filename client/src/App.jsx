f61ffc7711116b14c5c228924beff1413dfb3060
Micah Jaffe <micah.jaffe95@gmail.com>
2019-01-03 10:12:21 -0800

    Add store to window for testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,19 @@ window.axios = axios;
 import Root from './components/Root';
 import { setCurrentUser } from './utils/set_current_user';
 
+// TESTING
+import configureStore from './reducers/root_reducer';
+//
+
 document.addEventListener('DOMContentLoaded', () => {
+
+   // TESTING
+   let store = configureStore();
+   window.getState = store.getState;
+   window.store = store;
+   //
+
+
    ReactDom.render(
       <Root store={setCurrentUser()} />,
       document.getElementById('root')
