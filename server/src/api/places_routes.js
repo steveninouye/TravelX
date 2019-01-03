c2e754bd06df9c34766d6a23622defaded587a43
Steven Inouye <steveninouye@msn.com>
2019-01-02 20:20:07 -0800

    Add routes to request attraction and city from google places

--- server/src/api/places_routes.js
+++ server/src/api/places_routes.js
@@ -9,16 +9,33 @@ import { location, attraction } from './temp_api';
 const places = express.Router();
 
 places.get(
-   '/:city',
+   '/city/:city',
    passport.authenticate('jwt', { session: false }),
    (req, res) => {
-      const city = req.params;
+      const { city } = req.params;
       rp(
          `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${city}+point+of+interest&language=en&key=${googleApi}`
       )
          .then((res) => {
-            const {next_page_token, results}
-            let str = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoReference}&key=${googleApi}`;
+            const { results, next_page_token } = res;
+            res.json(res);
+         })
+         .catch((err) => {
+            res.json('Google API could not be reached');
+         });
+   }
+);
+
+places.get(
+   '/attraction/:id',
+   passport.authenticate('jwt', { session: false }),
+   (req, res) => {
+      const { id } = req.params;
+      rp(
+         `https://maps.googleapis.com/maps/api/place/details/json?placeid=${id}&fields=name,rating,formatted_phone_number,icon,photo,type,vicinity,url,opening_hours,website,price_level,rating,reviews&key=${googleApi}`
+      )
+         .then((res) => {
+            const { results, next_page_token } = res;
             res.json(res);
          })
          .catch((err) => {
