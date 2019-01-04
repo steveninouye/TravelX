8d22b8cb3137c66873381f221846e12134fe8e4b
Josh Stroud <josh.e.stroud@gmail.com>
2019-01-04 10:01:06 -0800

    Add feature images to landing page

--- client/src/components/homepage/LandingPage.jsx
+++ client/src/components/homepage/LandingPage.jsx
@@ -4,6 +4,11 @@ import landingPhoto from "../../img/backgrounds/city_at_dawn.jpg";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 
+import feature1 from "../../img/features/map-icon.png";
+import feature2 from "../../img/features/attraction-icon.png";
+import feature3 from "../../img/features/calendar-icon.png";
+import feature4 from "../../img/features/trip-icon.png";
+
 class LandingPage extends Component {
   render() {
     return (
@@ -15,18 +20,42 @@ class LandingPage extends Component {
         <section>
           <Grid container spacing={16} justify="center">
             <Grid item>
-              <img src="" alt="" />
+              <img src={feature1} alt="" />
             </Grid>
             <Grid item>
               <Typography component="h3" color="textPrimary">
                 Choose a destination
               </Typography>
             </Grid>
+            <Grid item>
+              <img src={feature2} alt="" />
+            </Grid>
+            <Grid item>
+              <Typography component="h3" color="textPrimary">
+                Discover exciting attractions
+              </Typography>
+            </Grid>
+            <Grid item>
+              <img src={feature3} alt="" />
+            </Grid>
+            <Grid item>
+              <Typography component="h3" color="textPrimary">
+                Create a day-by-day plan
+              </Typography>
+            </Grid>
+            <Grid item>
+              <img src={feature4} alt="" />
+            </Grid>
+            <Grid item>
+              <Typography component="h3" color="textPrimary">
+                Book your trip
+              </Typography>
+            </Grid>
           </Grid>
         </section>
         <section>
-          <Typography variant="h2" gutterBottom>
-            h2. Heading
+          <Typography variant="h2" gutterBottom align="center">
+            About the project
           </Typography>
           <div />
           <div />
