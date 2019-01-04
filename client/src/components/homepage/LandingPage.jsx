679d72398788e74e440bccf9de69bb2a5cbea4f7
Josh Stroud <josh.e.stroud@gmail.com>
2019-01-04 11:00:53 -0800

    Add team photos to landing page

--- client/src/components/homepage/LandingPage.jsx
+++ client/src/components/homepage/LandingPage.jsx
@@ -9,6 +9,11 @@ import feature2 from "../../img/features/attraction-icon.png";
 import feature3 from "../../img/features/calendar-icon.png";
 import feature4 from "../../img/features/trip-icon.png";
 
+import team1 from "../../img/team/micah.jpeg";
+import team2 from "../../img/team/steven.jpeg";
+import team3 from "../../img/team/taran.jpeg";
+import team4 from "../../img/team/josh.jpeg";
+
 class LandingPage extends Component {
   render() {
     return (
@@ -56,6 +61,25 @@ class LandingPage extends Component {
         <section>
           <Typography variant="h2" gutterBottom align="center">
             About the project
+          </Typography>
+          <Grid container spacing={16} justify="center">
+            <Grid item>
+              <img src={team1} alt="" />
+            </Grid>
+            <Grid item>
+              <img src={team2} alt="" />
+            </Grid>
+            <Grid item>
+              <img src={team3} alt="" />
+            </Grid>
+            <Grid item>
+              <img src={team4} alt="" />
+            </Grid>
+          </Grid>
+          <Typography variant="p" gutterBottom align="center">
+            This project was created by a team of software developers at App
+            Academy, the #1 coding bootcamp with a 3% acceptance rate. Icons or
+            links here
           </Typography>
           <div />
           <div />
