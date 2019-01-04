ec0244985247f81071d1fcdb34b864d3cabfa5c2
Josh Stroud <josh.e.stroud@gmail.com>
2019-01-04 11:14:49 -0800

    Fix merge conflict

--- client/src/components/Root.jsx
+++ client/src/components/Root.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-// import { Provider } from 'react-redux';.
+import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 
 import Router from "./Router";
 
 const Root = ({ store }) => (
   // <Provider store={store}>
-  <HashRouter>
-    <Router />
-  </HashRouter>
-  // </Provider>
+  <Provider store={store}>
+    <HashRouter>
+      <Router />
+    </HashRouter>
+  </Provider>
 );
 
 export default Root;
