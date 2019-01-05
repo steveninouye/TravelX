dd09c94ab8d596b8f38c0064f1ad8556e9174655
Micah Jaffe <micah.jaffe95@gmail.com>
2019-01-05 14:26:06 -0800

    Add plan show action buttons

--- client/src/components/plans/plan_show.jsx
+++ client/src/components/plans/plan_show.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from "@material-ui/core/Button";
 
 export default class PlanShow extends React.Component {
   componentDidMount() {
@@ -7,7 +8,23 @@ export default class PlanShow extends React.Component {
 
   render() {
     return (
-      <div>Plan Show</div>
+      <div>
+        <div>Nav Bar Placeholder</div>
+        <img src="https://cdn.pixabay.com/photo/2016/11/18/19/01/paris-1836415__340.jpg" alt=""/>
+        <div>Schedule/Map Placeholder</div>
+        <Button 
+          color="primary"
+          variant="contained"
+        >
+          Save
+        </Button>
+        <Button
+          color="primary"
+          variant="contained"
+        >
+          New Plan
+        </Button>
+      </div>
     );
   }
 };
