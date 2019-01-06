40f6c8dec10f562a3893c53954138e18041c5f9f
Micah Jaffe <micah.jaffe95@gmail.com>
2019-01-05 17:16:45 -0800

    Add photo to schedule item

--- client/src/components/plans/schedule_item.jsx
+++ client/src/components/plans/schedule_item.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
 const ScheduleItem = (props) => {
   const { attraction } = props;
 
   return (
-    <div>{attraction.name}</div>
+    <div>
+      <Typography>{attraction.name}</Typography>
+      {attraction.rating}
+      {attraction.formatted_address}
+      <img src={attraction.icon}></img>
+      {attraction.photos[0].photo_reference}
+      <img src="https://media-cdn.tripadvisor.com/media/photo-s/12/f5/f1/8d/eiffel-tower-summit-priority.jpg" alt=""/>
+    </div>
   );
 }
 
