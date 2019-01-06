5e6ba84c09f9650e74e23c5ee52d40bcc6139630
Micah Jaffe <micah.jaffe95@gmail.com>
2019-01-06 13:06:38 -0800

    Fix button styling on plan show nav bar

--- client/src/components/plans/plan_show_nav_bar.jsx
+++ client/src/components/plans/plan_show_nav_bar.jsx
@@ -5,6 +5,8 @@ import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar'
 import ScheduleShowContainer from "./schedule_show_container";
 
 function TabContainer(props) {
@@ -23,10 +25,23 @@ const styles = theme => ({
   root: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.paper
+  },
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '33%',
+    padding: '0'
+  },
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  button: {
+    width: '42%'
   }
 });
 
-class SimpleTabs extends React.Component {
+class PlanShowNavBar extends React.Component {
   constructor(props) {
     super(props);
 
@@ -48,21 +63,38 @@ class SimpleTabs extends React.Component {
     return (
       <div className={classes.root}>
         <AppBar position="static">
-          <Tabs value={value} onChange={this.handleChange}>
-            <Tab label="Item One" />
-            <Tab label="Item Two" />
-          </Tabs>
+          <Toolbar className={classes.toolbar}>
+            <Tabs value={value} onChange={this.handleChange}>
+              <Tab label="Schedule" />
+              <Tab label="Map" />
+            </Tabs>
+            <Toolbar className={classes.buttonContainer}>
+              <Button
+                color="secondary"
+                variant="contained"
+                className={classes.button}
+              >
+                Save
+              </Button>
+              <Button
+                color="secondary"
+                variant="contained"
+                className={classes.button}
+              >
+                New Plan
+              </Button>
+            </Toolbar>
+          </Toolbar>
         </AppBar>
         {value === 0 && <TabContainer><ScheduleShowContainer/></TabContainer>}
         {value === 1 && <TabContainer>Item Two</TabContainer>}
-        {value === 2 && <TabContainer>Item Three</TabContainer>}
       </div>
     );
   }
 }
 
-SimpleTabs.propTypes = {
+PlanShowNavBar.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(SimpleTabs);
+export default withStyles(styles)(PlanShowNavBar);
