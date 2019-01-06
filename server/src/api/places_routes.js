02b03e785a1551647ab2360b42e358a0892def95
Steven Inouye <steveninouye@msn.com>
2019-01-05 18:00:41 -0800

    Remove passport authentication from places routes

--- server/src/api/places_routes.js
+++ server/src/api/places_routes.js
@@ -10,51 +10,39 @@ import { savePhoto, getCityAttractions } from '../utils/db_utils';
 
 const places = express.Router();
 
-places.post(
-   '/city',
-   passport.authenticate('jwt', { session: false }),
-   (req, res) => {
-      const { city } = req.body;
-      getCityAttractions(city)
-         .then((attractions) => {
-            let randAttractions = [1, 2, 3, 4, 5];
-            randAttractions = randAttractions.map((_, idx) => {
-               console.log('this is running');
-               let attractionIdx = randNum(attractions.length - idx, 0);
-               return attractions.splice(attractionIdx, 1);
-            });
-            res.json(randAttractions);
-         })
-         .catch((err) => {
-            res.status(500).json('Googe API could not be reached');
+places.post('/city', (req, res) => {
+   const { city } = req.body;
+   getCityAttractions(city)
+      .then((attractions) => {
+         let randAttractions = [1, 2, 3, 4, 5];
+         randAttractions = randAttractions.map((_, idx) => {
+            console.log('this is running');
+            let attractionIdx = randNum(attractions.length - idx, 0);
+            return attractions.splice(attractionIdx, 1);
          });
-   }
-);
+         res.json(randAttractions);
+      })
+      .catch((err) => {
+         res.status(500).json('Googe API could not be reached');
+      });
+});
 
-places.get(
-   '/attraction/:id',
-   passport.authenticate('jwt', { session: false }),
-   (req, res) => {
-      const { id } = req.params;
-      rp(attractionUrl(id))
-         .then((json) => {
-            const attraction = JSON.parse(json).result;
-            res.json(attraction);
-         })
-         .catch((err) => {
-            console.log(err);
-            res.status(500).json('Google API could not be reached');
-         });
-   }
-);
+places.get('/attraction/:id', (req, res) => {
+   const { id } = req.params;
+   rp(attractionUrl(id))
+      .then((json) => {
+         const attraction = JSON.parse(json).result;
+         res.json(attraction);
+      })
+      .catch((err) => {
+         console.log(err);
+         res.status(500).json('Google API could not be reached');
+      });
+});
 
-places.get(
-   '/photo/:photoRef',
-   passport.authenticate('jwt', { session: false }),
-   (req, res) => {
-      const { photoRef } = req.params;
-      savePhoto(photoRef, true, true);
-   }
-);
+places.get('/photo/:photoRef', (req, res) => {
+   const { photoRef } = req.params;
+   savePhoto(photoRef, true, true);
+});
 
 export default places;
