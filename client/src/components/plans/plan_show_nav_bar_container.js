d62294b9eac12ed66a342e6e7389922406165de8
Taran Cacacho <tarancacacho@gmail.com>
2019-01-06 15:46:48 -0800

    Add state to fetch particular plan and dispatch to call fetchPlan

--- client/src/components/plans/plan_show_nav_bar_container.js
+++ client/src/components/plans/plan_show_nav_bar_container.js
@@ -1,13 +1,17 @@
 import { connect } from "react-redux";
 import PlanShowNavBar from "./plan_show_nav_bar";
 import { openModal } from '../../actions/modal_actions';
+import { selectPlan } from '../../reducers/selectors';
+import { fetchPlan } from "../../actions/plan_actions";
 
-const mapStateToProps = ({ session }) => ({
-  session
+const mapStateToProps = (state) => ({
+  planId: parseInt(match.params.planId),
+  plan: selectPlan(state.entities, planId),
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  openModal: (modal) => dispatch(openModal(modal))
+  openModal: (modal) => dispatch(openModal(modal)),
+  fetchPlan: (id) => dispatch(fetchPlan(id))
 });
 
 export default connect(
