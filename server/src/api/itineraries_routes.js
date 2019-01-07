cb5bed80f6dfcc312ad2aaa4deb5406cbc3cc56a
Steven Inouye <steveninouye@msn.com>
2019-01-06 17:05:10 -0800

    Configure get request to get itineraries of user

--- server/src/api/itineraries_routes.js
+++ server/src/api/itineraries_routes.js
@@ -8,6 +8,25 @@ import ItineraryPackage from '../../dist/models/ItineraryPackage';
 
 const itineraries = express.Router();
 
+itineraries.get(
+   '/',
+   passport.authenticate('jwt', { session: false }),
+   (req, res) => {
+      User.findById(req.user._id)
+         .populate({
+            path: 'itinerary_packages',
+            populate: {
+               path: 'attractions',
+               populate: {
+                  path: 'city'
+               }
+            }
+         })
+         .then((user) => res.send(user))
+         .catch((err) => res.status(500).send(err));
+   }
+);
+
 itineraries.post(
    '/',
    passport.authenticate('jwt', { session: false }),
