a1dd3fc22f42dff95090d00ff915b487e68c51d0
Taran Cacacho <tarancacacho@gmail.com>
2019-01-06 16:07:40 -0800

    Render NavBar to PlanShow page

--- client/src/components/plans/plan_show.jsx
+++ client/src/components/plans/plan_show.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PlanShowHero from './plan_show_hero';
 import PlanShowNavBarContainer from './plan_show_nav_bar_container';
+import NavBar from './navbar'
 
 export default class PlanShow extends React.Component {
   componentDidMount() {
@@ -11,7 +12,7 @@ export default class PlanShow extends React.Component {
   render() {
     return (
       <div>
-        <div style={{ height: '50px', background: 'gray' }}>Nav Bar Placeholder</div>
+        <NavBar/>
         <PlanShowHero city={"Paris"}/>
         <PlanShowNavBarContainer />
       </div>
