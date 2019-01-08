d2dfce2b3c3d0744bd2077115e6641ee06e5b0f5
Steven Inouye <steveninouye@msn.com>
2019-01-08 13:57:38 -0800

    Handle no attraction results if city has no attractions

--- server/src/utils/db_utils.js
+++ server/src/utils/db_utils.js
@@ -11,160 +11,152 @@ import { cityUrl, unsplashUrl } from '../utils/api_urls';
 import { unzipSync } from 'zlib';
 
 export const getItinerary = (attractions) =>
-   new Promise((resolve, reject) => {
-      ItineraryPackage.findOne({ attractions })
-         .populate({
-            path: 'attractions',
-            populate: { path: 'city' }
-         })
-         .then((itinerary) => {
-            if (itinerary) {
-               resolve(itinerary);
-            } else {
-               const newItinerary = new ItineraryPackage({ attractions });
-               newItinerary
-                  .save()
-                  .then((itinerary) => {
-                     ItineraryPackage.findById(itinerary._id)
-                        .populate({
-                           path: 'attractions',
-                           populate: { path: 'city' }
-                        })
-                        .then((itinerary) => {
-                           resolve(itinerary);
-                        });
-                  })
-                  .catch((err) => {
-                     resolve(err);
-                  });
-            }
-         })
-         .catch((err) => reject(err));
-   });
+  new Promise((resolve, reject) => {
+    ItineraryPackage.findOne({ attractions })
+      .populate({
+        path: 'attractions',
+        populate: { path: 'city' }
+      })
+      .then((itinerary) => {
+        if (itinerary) {
+          resolve(itinerary);
+        } else {
+          const newItinerary = new ItineraryPackage({ attractions });
+          newItinerary
+            .save()
+            .then((itinerary) => {
+              ItineraryPackage.findById(itinerary._id)
+                .populate({
+                  path: 'attractions',
+                  populate: { path: 'city' }
+                })
+                .then((itinerary) => {
+                  resolve(itinerary);
+                });
+            })
+            .catch((err) => {
+              resolve(err);
+            });
+        }
+      })
+      .catch((err) => reject(err));
+  });
 
 export const getCity = (cityName) =>
-   new Promise((resolve, reject) => {
-      City.findOne({ name: cityName })
-         .then((city) => {
-            if (city) {
-               resolve({ city, isFound: true });
-            } else {
-               const cityQuery = cityName.split(' ').join('%20');
-               rp(unsplashUrl(cityQuery)).then((json) => {
-                  const { results } = JSON.parse(json);
-                  const photos = results.map((result) => result.urls.full);
-                  const newCity = new City({ name: cityName, photos });
-                  newCity.save().then((city) => {
-                     resolve({ city, isFound: false });
-                  });
-               });
-            }
-         })
-         .catch((err) => reject(err));
-   });
+  new Promise((resolve, reject) => {
+    City.findOne({ name: cityName })
+      .then((city) => {
+        if (city) {
+          resolve({ city, isFound: true });
+        } else {
+          const cityQuery = cityName.split(' ').join('%20');
+          rp(unsplashUrl(cityQuery)).then((json) => {
+            const { results } = JSON.parse(json);
+            const photos = results.map((result) => result.urls.full);
+            const newCity = new City({ name: cityName, photos });
+            newCity.save().then((city) => {
+              resolve({ city, isFound: false });
+            });
+          });
+        }
+      })
+      .catch((err) => reject(err));
+  });
 
 export const requestCityAttractions = (url, city, resolve, reject) => {
-   rp(url)
-      .then((json1) => {
-         console.log('json1: ', json1);
-         let { results, next_page_token } = JSON.parse(json1);
-         console.log('results: ', results);
-         if (results.length > 0) {
-            const attractions = results.map((attraction) => {
-               attraction.city = city._id;
-               return attraction;
-            });
-            Attraction.create(attractions).then((attractions) => {
-               if (next_page_token) {
-                  setTimeout(() => {
-                     rp(`${url}&pagetoken=${next_page_token}`).then((json2) => {
-                        let { results, next_page_token } = JSON.parse(json2);
-                        if (results.length > 0) {
-                           const attractions = results.map((attraction) => {
-                              attraction.city = city._id;
-                              return attraction;
-                           });
-                           Attraction.create(attractions).then(() => {
-                              if (next_page_token) {
-                                 setTimeout(() => {
-                                    rp(
-                                       `${url}&pagetoken=${next_page_token}`
-                                    ).then((json2) => {
-                                       let { results } = JSON.parse(json2);
-                                       if (results.length > 0) {
-                                          const attractions = results.map(
-                                             (attraction) => {
-                                                attraction.city = city._id;
-                                                return attraction;
-                                             }
-                                          );
-                                          Attraction.create(attractions)
-                                             .then(() =>
-                                                console.log(
-                                                   'Attractions created'
-                                                )
-                                             )
-                                             .catch(() =>
-                                                console.log('error occured')
-                                             );
-                                       }
-                                    });
-                                 }, 5000);
-                              }
-                           });
-                        }
-                     });
-                  }, 5000);
-               }
-               resolve(attractions);
-            });
-         } else {
-            reject('No Results found');
-         }
-      })
-      .catch((err) => {
-         reject(err);
-      });
+  rp(url)
+    .then((json1) => {
+      console.log('json1: ', json1);
+      let { results, next_page_token } = JSON.parse(json1);
+      console.log('results: ', results);
+      if (results.length > 0) {
+        const attractions = results.map((attraction) => {
+          attraction.city = city._id;
+          return attraction;
+        });
+        Attraction.create(attractions).then((attractions) => {
+          if (next_page_token) {
+            setTimeout(() => {
+              rp(`${url}&pagetoken=${next_page_token}`).then((json2) => {
+                let { results, next_page_token } = JSON.parse(json2);
+                if (results.length > 0) {
+                  const attractions = results.map((attraction) => {
+                    attraction.city = city._id;
+                    return attraction;
+                  });
+                  Attraction.create(attractions).then(() => {
+                    if (next_page_token) {
+                      setTimeout(() => {
+                        rp(`${url}&pagetoken=${next_page_token}`).then(
+                          (json2) => {
+                            let { results } = JSON.parse(json2);
+                            if (results.length > 0) {
+                              const attractions = results.map((attraction) => {
+                                attraction.city = city._id;
+                                return attraction;
+                              });
+                              Attraction.create(attractions)
+                                .then(() => console.log('Attractions created'))
+                                .catch(() => console.log('error occured'));
+                            }
+                          }
+                        );
+                      }, 5000);
+                    }
+                  });
+                }
+              });
+            }, 5000);
+          }
+          resolve(attractions);
+        });
+      } else {
+        resolve([]);
+      }
+    })
+    .catch((err) => {
+      reject(err);
+    });
 };
 
 export const getCityAttractions = (cityName) =>
-   new Promise((resolve, reject) => {
-      let url = cityUrl(cityName);
-      getCity(cityName).then(({ city, isFound }) => {
-         if (isFound) {
-            Attraction.find({ city: city._id })
-               .then((attractions) => {
-                  resolve(attractions);
-               })
-               .catch((err) => {
-                  reject(err);
-               });
-         } else {
-            requestCityAttractions(url, city, resolve, reject);
-         }
-      });
-   });
+  new Promise((resolve, reject) => {
+    let url = cityUrl(cityName);
+    getCity(cityName).then(({ city, isFound }) => {
+      if (isFound) {
+        Attraction.find({ city: city._id })
+          .then((attractions) => {
+            resolve(attractions);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      } else {
+        requestCityAttractions(url, city, resolve, reject);
+      }
+    });
+  });
 
 export const savePhoto = (
-   req,
-   res,
-   photoRef,
-   fileResponseBoolean,
-   errorResponseBoolean
+  req,
+  res,
+  photoRef,
+  fileResponseBoolean,
+  errorResponseBoolean
 ) => {
-   const filePath = resolve(__dirname, `../img/${photoRef}.jpg`);
-   if (fs.existsSync(filePath)) {
-      if (fileResponseBoolean) res.sendFile(filePath);
-   } else {
-      const file = fs.createWriteStream(filePath);
-      const photoUrl = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoRef}&key=${googleApi}`;
-      request(photoUrl).on('response', (response) => {
-         response.pipe(file);
-         file.on('finish', () => {
-            file.close(() => {
-               if (fileResponseBoolean) res.sendFile(filePath);
-            });
-         });
+  const filePath = resolve(__dirname, `../img/${photoRef}.jpg`);
+  if (fs.existsSync(filePath)) {
+    if (fileResponseBoolean) res.sendFile(filePath);
+  } else {
+    const file = fs.createWriteStream(filePath);
+    const photoUrl = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoRef}&key=${googleApi}`;
+    request(photoUrl).on('response', (response) => {
+      response.pipe(file);
+      file.on('finish', () => {
+        file.close(() => {
+          if (fileResponseBoolean) res.sendFile(filePath);
+        });
       });
-   }
+    });
+  }
 };
