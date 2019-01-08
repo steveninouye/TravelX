61944c1b30ce5f7b4456a6fec43a28666e5e91c5
Taran Cacacho <tarancacacho@gmail.com>
2019-01-08 15:13:12 -0800

    Export grid container with Router

--- client/src/components/plans/plans-index/plans_index_grid_container.jsx
+++ client/src/components/plans/plans-index/plans_index_grid_container.jsx
@@ -1,16 +1,18 @@
 import { connect } from "react-redux";
 import PlansIndexGrid from "./plans_index_grid";
 import { getItineraries } from "../../../actions/itinerary_actions";
+import { withRouter } from "react-router-dom";
 
-const mapStateToProps = state => ({
-  itineraries: state.entities.itineraries
+const mapStateToProps = (state, ownProps) => ({
+  itineraries: state.entities.itineraries,
+  history: ownProps.history
 });
 
 const mapDispatchToProps = dispatch => ({
   getItineraries: () => dispatch(getItineraries())
 });
 
-export default connect(
+export default withRouter(connect(
   mapStateToProps,
   mapDispatchToProps
-)(PlansIndexGrid);
+)(PlansIndexGrid));
