9d765884bdd78f72fd23316a58d271ec9c70d324
Steven Inouye <steveninouye@msn.com>
2019-01-08 12:15:06 -0800

    Complete login modal feature to update session/login

--- client/src/reducers/modal_reducer.js
+++ client/src/reducers/modal_reducer.js
@@ -1,14 +1,15 @@
-import { OPEN_MODAL, CLOSE_MODAL } from "../actions/modal_actions";
+import { OPEN_MODAL, CLOSE_MODAL } from '../actions/modal_actions';
 
 const modalReducer = (state = null, action) => {
-  switch (action.type) {
-    case OPEN_MODAL:
-      return action.name;
-    case CLOSE_MODAL:
-      return null;
-    default:
-      return state;
-  }
+  //  debugger;
+   switch (action.type) {
+      case OPEN_MODAL:
+         return action.name;
+      case CLOSE_MODAL:
+         return null;
+      default:
+         return state;
+   }
 };
 
 export default modalReducer;
