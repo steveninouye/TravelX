fbccc5742cbeae5e70cd4946b4f2983208ad1558
Taran Cacacho <tarancacacho@gmail.com>
2019-01-08 12:23:09 -0800

    Fix bang typo

--- client/src/utils/route_util.js
+++ client/src/utils/route_util.js
@@ -4,7 +4,7 @@ import { Route, Redirect, withRouter } from 'react-router-dom';
 
 const Auth = ({ component: Component, path, loggedIn, exact }) => (
   <Route path={path} exact={exact} render={(props) => (
-    !loggedIn ? (
+    loggedIn ? (
       <Component {... props} />
     ) : (
       <Redirect to ="/" />
@@ -26,7 +26,7 @@ const Protected = ({ component: Component, loggedIn, ...rest }) => (
 );
 
 const mapStateToProps = state => (
-  {loggedIn: !state.session}
+  {loggedIn: state.session}
 )
 
 export const AuthRoute = withRouter(connect(mapStateToProps)(Auth));
