3d7386353a03a54fa901d82bbbbf38e60550d938
Josh Stroud <josh.e.stroud@gmail.com>
2019-01-08 11:41:12 -0800

    Reset scroll of plan show page when visiting from another page that is partially scrolled

--- client/src/components/plans/plan_show.jsx
+++ client/src/components/plans/plan_show.jsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import PlanShowHero from './plan_show_hero';
-import PlanShowNavBarContainer from './plan_show_nav_bar_container';
-import NavBar from './navbar';
+import React from "react";
+import ReactDOM from "react-dom";
+import PlanShowHero from "./plan_show_hero";
+import PlanShowNavBarContainer from "./plan_show_nav_bar_container";
+import NavBar from "./navbar";
 
 export default class PlanShow extends React.Component {
-  componentDidMount() {  
+  componentDidMount() {
     const { getRandCityItinerary, city } = this.props;
 
-    getRandCityItinerary(city.name).then(
-      res => this.props.history.push(`/plans/${res.itinerary._id}`)
+    let node = ReactDOM.findDOMNode(this);
+    while (node.parentNode) {
+      node.scrollTop = 0;
+      node = node.parentNode;
+    }
+
+    getRandCityItinerary(city.name).then(res =>
+      this.props.history.push(`/plans/${res.itinerary._id}`)
     );
 
     document.title = `TravelX | ${city.name}`;
-  };
+  }
 
-   render() {
-      return (
-         <div>
-            <NavBar />
-            <PlanShowHero city={this.props.city} />
-            <PlanShowNavBarContainer />
-         </div>
-      );
-   }
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <PlanShowHero city={this.props.city} />
+        <PlanShowNavBarContainer />
+      </div>
+    );
+  }
 }
