569ce7b171312ba7ac24908122a9b048f7724e45
Taran Cacacho <tarancacacho@gmail.com>
2019-01-07 18:58:05 -0800

    Add receiveItineraries action creator

--- client/src/actions/itinerary_actions.js
+++ client/src/actions/itinerary_actions.js
@@ -1,11 +1,17 @@
 import * as ItineraryAjaxUtil from '../utils/itinerary_ajax_utils';
 
 export const RECEIVE_ITINERARY = 'RECEIVE_ITINERARY';
+export const RECEIVE_ITINERARIES = 'RECEIVE_ITINERARIES';
 export const RECEIVE_ITINERARY_ERRORS = 'RECEIVE_ITINERARY_ERRORS';
 
 export const receiveCityItinerary = (itinerary) => ({
    type: RECEIVE_ITINERARY,
    itinerary
+});
+
+export const receiveItineraries = (itineraries) => ({
+  type: RECEIVE_ITINERARIES,
+  itineraries
 });
 
 export const receiveGetItineraryErrors = (errors) => ({
@@ -20,7 +26,14 @@ export const getRandCityItinerary = (city) => (dispatch) => {
 };
 
 export const getItinerary = (id) => (dispatch) => {
-   return ItineraryAjaxUtil.getItinerary(id)
-     .then(itinerary => dispatch(receiveCityItinerary(itinerary.data)))
-     .catch(err => dispatch(receiveGetItineraryErrors(err)));
+  return ItineraryAjaxUtil.getItinerary(id)
+    .then(itinerary => dispatch(receiveCityItinerary(itinerary.data)))
+    .catch(err => dispatch(receiveGetItineraryErrors(err)));
+};
+
+// Do not need to pass in UserID
+export const getItineraries = () => (dispatch) => {
+  return ItineraryAjaxUtil.getItineraries()
+    .then(itineraries => dispatch(receiveItineraries(itineraries.data)))
+    .catch(err => dispatch(receiveGetItineraryErrors(err)));
 };
